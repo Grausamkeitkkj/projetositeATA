a456483a960a01a25db01778fbb9ac24b9f0f2f1
grausamkeitkkj <diogorschultz2@gmail.com>
2024-09-30 22:32:24 -0300

    Adicionando função pra deixar o menu vermelho ao passar mouse

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 // Função para expandir e recolher o menu
 var menuItem = document.querySelectorAll('.item-menu')
-var subMenu = document.querySelectorAll('.sub-menu')
+var subMenu = document.querySelectorAll('.item-sub-menu')
 
 function selectLink(){
     menuItem.forEach((item)=>
@@ -9,7 +9,15 @@ function selectLink(){
     this.classList.add('active')
 }
 
+function selectLink2(){
+    subMenu.forEach((item)=>
+        item.classList.remove('active')
+    )
+    this.classList.add('active')
+}
+
 menuItem.forEach((item)=>item.addEventListener('click', selectLink))
+subMenu.forEach((item)=>item.addEventListener('click', selectLink2))
 
 var btnExp = document.querySelector('#exp-button')
 var menuSide = document.querySelector('.side-menu')
